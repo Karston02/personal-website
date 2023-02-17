9cd392034acccb1c0e5054c72d96bc50bb497f61
Karston Kuciemba <kuciembakarston@gmail.com>
2023-02-17 14:17:40 -0600

    added mobile menu
    styled the mobile menu and have js effect for opening/closing the hamburger

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // function to get rid of preloader after it loads
-var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
-    loader.style.display = "none";
+var loader = document.getElementById('preloader');
+window.addEventListener('load', function(){
+    loader.style.display = 'none';
 })
+
+// function for mobile menu click effect
+const mobileMenu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu');
+
+mobileMenu.addEventListener('click', function(){
+    mobileMenu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
